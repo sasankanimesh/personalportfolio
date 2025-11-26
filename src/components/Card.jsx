e68cbf9cd6f0b7e8f709b7c37847c2fff9beec7d
sasankanimesh <140873068+sasankanimesh@users.noreply.github.com>
2025-11-26 18:47:08 +0530

    Delete src directory

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Card({ className = "", children }) {
-  return (
-    <div className={`rounded-2xl bg-white shadow-md ${className}`}>
-      {children}
-    </div>
-  );
-}
